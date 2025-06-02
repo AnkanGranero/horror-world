8124fd14c68e2bf96f7a4c71eac2dd95b02eedb8
Andreas Granér <andreas@graner.me>
2025-06-02 20:10:34 +0200

    fix: updated fetch endpoint to use with_origin_country instead of region

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -1,8 +1,9 @@
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const region = searchParams.get("region");
+  
 
-  const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=27&region=${region}`);
+  const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=27&&with_origin_country=${region}`);
   const data = await res.json();
 
   return Response.json(data);
