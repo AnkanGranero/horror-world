2c22c8336052d5491d13df69cddf3c05c8d67f77
Andreas Granér <andreas@graner.me>
2025-06-02 13:11:11 +0200

    VIP: Add extended info movie card

--- src/types/country.ts
+++ src/types/country.ts
@@ -2,11 +2,9 @@ export type Country = {
     name: { common: string };
     cca2: string;
     flags: { png: string; svg: string };
-    // + andra fält du behöver
 };
 
 export type CountryContextType = {
     selectedCountry: Country | null;
     setSelectedCountry: (country: Country | null) => void;
-    
 };
