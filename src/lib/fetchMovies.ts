99285e1258428b8a88697543b23762c262abc116
Andreas Granér <Andreas@graner.me>
2025-06-04 10:03:23 +0200

    fix: extra error handling for fetchMovies

--- src/lib/fetchMovies.ts
+++ src/lib/fetchMovies.ts
@@ -4,6 +4,16 @@ import { Country } from "@/types/country";
 export async function fetchMovies(selectedCountry: Country): Promise<Movie[]> {
   const res = await fetch(`/api/movies?region=${selectedCountry.cca2}`);
   if (!res.ok) throw new Error("Failed to fetch movies");
+  
   const data = await res.json();
+  
+  if (data.success === false) {
+    throw new Error(data.status_message || "Unknown TMDB error");
+  }
+
+  if (!Array.isArray(data.results)) {
+    throw new Error("API response missing 'results'");
+  }
+
   return data.results;
 }
